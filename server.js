000fad27191d08a61914a791f03e7477fef67894
Gabriel De Liz Do Nascimento <gabriel.nascimento@renalvida.org.br>
2025-03-03 15:39:02 -0300

    cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://jogo-velha-rh2b.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true
     }
